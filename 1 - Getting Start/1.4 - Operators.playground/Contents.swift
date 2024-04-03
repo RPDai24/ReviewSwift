0b8198ace3886a8fe5671cde4f452c6aded24d47
RPD <hupochuan80@icloud.com>
2024-04-03 19:07:03 +0100

    operators

---
+++ 1 - Getting Start/1.4 - Operators.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+// Assign a value
+let favoritePerson = "Guo"
+var shoeSize = 8
+shoeSize = 9
+
+// Basic Arithmetic
+var opponentScore = 3 * 8
+var myScore = 100 / 4
+var totalScore = opponentScore + myScore
+myScore = myScore + 3
+
+let totalDistance = 3.9
+var distanceTraveled = 1.2
+var remainingDistance = totalDistance - distanceTraveled
+
+//let x = 51
+//let y = 4
+//let z = x / y
+//
+//let x: Double = 51
+//let y: Double = 4
+//let z = x / y
+
+// Compound Assignment
+myScore += 3
+myScore -= 5
+myScore *= 2
+myScore /= 2
+
+// Rmmainder Operator
+let dividend = 10
+let divisor = 3
+let quotient = dividend / divisor
+let remainder = dividend % divisor
+
+// Numeric Type Conversion
+let x = 3
+let y = 0.1415927
+let pi = Double(x) + y
+
