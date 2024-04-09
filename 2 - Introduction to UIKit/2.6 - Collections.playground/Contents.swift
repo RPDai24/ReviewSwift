26549fd796ba4dd9471469258143ba9c68cb275a
RPD <hupochuan80@icloud.com>
2024-04-09 14:35:28 +0100

    Collections

---
+++ 2 - Introduction to UIKit/2.6 - Collections.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Arrays
+var name = ["Anne", "Gary", "Keith", "d"]
+var numbers: [Int8] = [1, -3, 50, 72, -95, 115]
+
+let numbers2 = [2, 3, 4, 5]
+if numbers2.contains(5) {
+    print("There is a 5")
+}
+
+// Array types
+var myArray: [Int] = [] // traditional type annotation
+var myArray2: Array<Int> = [] // a special collection type annotation
+var myArray3 = [Int]()
+
+// Working with Arrays
+
+
+
+
