039881a7886c10c4eb544a52a925011deb6a39bc
RP <hupochuan80@icloud.com>
2024-07-25 15:30:14 +0100

    Customize views with properties

--- Develop in Swift/WeatherForecast/WeatherForecast/ContentView.swift
+++ Develop in Swift/WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
-            DayForecast(day: "Tue", high: 68, low: 55)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            DayForecast(day: "Tue", isRainy: true, high: 68, low: 55)
         }
     }
 }
@@ -22,16 +22,40 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
     
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("\(high)")
-            Text("\(low)")
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
             
         }
         .padding()
