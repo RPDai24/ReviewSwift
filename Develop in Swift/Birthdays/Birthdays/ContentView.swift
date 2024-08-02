5f764433f32460eb0b1a5d227223c75af87ad5c1
RP <hupochuan80@icloud.com>
2024-08-02 20:50:57 +0100

    Save Data 03

--- Develop in Swift/Birthdays/Birthdays/ContentView.swift
+++ Develop in Swift/Birthdays/Birthdays/ContentView.swift
@@ -6,21 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var friends: [Friend] = [
-        Friend(name: "Elton Lin", birthday: .now),
-        Friend(name: "John Court", birthday: Date(timeIntervalSince1970: 0))
-    ]
+    @Query(sort: \Friend.birthday) private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     
     @State private var newName: String = ""
     @State private var newDate = Date.now
     
     var body: some View {
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack {
+                    if friend.isBirthdayToday {
+                        Image(systemName: "birthday.cake")
+                    }
                     Text(friend.name)
+                        .bold(friend.isBirthdayToday)
                     Spacer()
                     Text(friend.birthday, format: .dateTime.month(.wide).day().year())
                 }
@@ -30,8 +33,27 @@ struct ContentView: View {
                 VStack(alignment: .center, spacing: 20) {
                     Text("New Birthday")
                         .font(.headline)
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        context.insert(newFriend)
+                        
+                        newName = ""
+                        newDate = .now
+                    }
+                    .bold()
                 }
+                .padding()
+                .background(.bar)
             }
+//            .task {
+//                context.insert(Friend(name: "Elton Lin", birthday: .now))
+//                context.insert(Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0)))
+//            }
+            
         }
 
     }
@@ -39,4 +61,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
