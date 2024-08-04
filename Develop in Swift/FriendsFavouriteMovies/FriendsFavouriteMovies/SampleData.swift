6124d6ba1ed05d34f7831cb86b0efe288f63e1f4
RP <hupochuan80@icloud.com>
2024-08-04 15:09:47 +0100

    Use a class to provide sample data

--- Develop in Swift/FriendsFavouriteMovies/FriendsFavouriteMovies/SampleData.swift
+++ Develop in Swift/FriendsFavouriteMovies/FriendsFavouriteMovies/SampleData.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+    static let shared = SampleData()
+    
+    let modelContainer: ModelContainer
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    private init() {
+        let schema = Schema([
+            Movie.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+}
