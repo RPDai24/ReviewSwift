963079dc35334a77f5756671c410dfd4210215bf
RP <hupochuan80@icloud.com>
2026-03-01 17:30:09 +0000

    Model data with custom types

---
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by hh on 01/03/2026.
+//
+
+import Foundation
---
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by hh on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
