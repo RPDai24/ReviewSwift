d953359a70b88e6a2e511d77388b662f95541a9c
RP <hupochuan80@icloud.com>
2026-03-04 15:56:51 +0000

    Add functionality with Swift Testing

---
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/GameState.swift
@@ -0,0 +1,8 @@
+//
+//  GameState.swift
+//  ScoreKeeper
+//
+//  Created by hh on 02/03/2026.
+//
+
+import Foundation
---
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,8 @@
+//
+//  Scoreboard.swift
+//  ScoreKeeper
+//
+//  Created by hh on 01/03/2026.
+//
+
+import Foundation
