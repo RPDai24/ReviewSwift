db94eba89d38c71b14a2052ae86c1f2ac9d24f5a
RP <hupochuan80@icloud.com>
2026-03-08 16:27:59 +0000

    Implement  the game logic

--- Develop in Swift/Alphabetizer/Alphabetizer/Models/Alphabetizer.swift
+++ Develop in Swift/Alphabetizer/Alphabetizer/Models/Alphabetizer.swift
@@ -20,12 +20,47 @@ class Alphabetizer {
         self.vocab = vocab
         startNewGame()
     }
+    // Alternates true and false
+    private var isAlphabetized = false
     
     /// Checks if tiles are in alphabetical order
     func submit() {
-        // TODO: Implement submit
+        // Check if the tiles are alphabetized
+        // TODO: Compare alphabetier order to position
+        isAlphabetized.toggle()
+        // If alphabetized, increment the score
+        if isAlphabetized {
+            score += 1
+        }
         
-        score += 1
+        // Update the message win or lose
+        message = isAlphabetized ? .youWin : .tryAgain
+        
+        // Flip over correct tiles
+        for tile in tiles {
+            // TODO: Check if this tile is in the correct position
+            let tileIsAlphabetized = isAlphabetized
+            tile.flipped = tileIsAlphabetized
+        }
+        
+        Task { @MainActor in
+            // Delay 2 seconds
+            try await Task.sleep(for: .seconds(2))
+            
+            // If alphabetized, generate new tiles
+            if isAlphabetized {
+                tiles.removeAll()
+                startNewGame()
+            }
+            
+            // Flip tiles back to words
+            for tile in tiles {
+                tile.flipped = false
+            }
+            
+            // Display instructions
+            message = .instructions
+        }
         
     }
     
