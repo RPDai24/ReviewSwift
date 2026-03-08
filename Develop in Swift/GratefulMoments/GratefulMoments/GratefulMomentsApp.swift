e0e6eec21d1c6bea7df2beba46b79cb767c50f51
RP <hupochuan80@icloud.com>
2026-03-08 21:05:15 +0000

    Create a project - GratefulMoments

---
+++ Develop in Swift/GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by hh on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
