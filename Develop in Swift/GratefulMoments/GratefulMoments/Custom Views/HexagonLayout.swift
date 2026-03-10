eac6bfc6cc3a07aa5c503d72b30758aa24c92eb8
RP <hupochuan80@icloud.com>
2026-03-10 14:51:01 +0000

    Display data in your custom component

--- Develop in Swift/GratefulMoments/GratefulMoments/Custom Views/HexagonLayout.swift
+++ Develop in Swift/GratefulMoments/GratefulMoments/Custom Views/HexagonLayout.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum HexagonLayout {
     case standard
@@ -17,6 +18,36 @@ enum HexagonLayout {
             return 200.0
         case .large:
             return 350.0
+        }
+    }
+    
+    var timestampBottomPadding: CGFloat {
+        0.08
+    }
+    
+    var textBottomPadding: CGFloat {
+        0.25
+    }
+    
+    var timestampHeigh: CGFloat {
+        size * (textBottomPadding - timestampBottomPadding)
+    }
+    
+    var titleFont: Font {
+        switch self {
+        case .standard:
+            return .headline
+        case .large:
+            return .title.bold()
+        }
+    }
+    
+    var bodyFont: Font {
+        switch self {
+        case .standard:
+            return .caption2
+        case .large:
+            return .body
         }
     }
 }
---
+++ Develop in Swift/GratefulMoments/GratefulMoments/Tabs/Moments/MomentHexagonView.swift
@@ -0,0 +1,77 @@
+//
+//  MomentHexagonView.swift
+//  GratefulMoments
+//
+//  Created by hh on 10/03/2026.
+//
+
+import SwiftUI
+
+struct MomentHexagonView: View {
+    var moment: Moment
+    @State var layout: HexagonLayout = .standard
+    
+    private var showImage: Bool {
+        moment.image != nil
+    }
+    
+    var body: some View {
+        Hexagon(layout: layout, moment: moment) {
+            hexagonContent()
+        }
+
+    }
+    
+    private func hexagonContent() -> some View {
+        ZStack(alignment: .bottom) {
+            if showImage {
+                Color.clear
+                contentStack()
+                    .frame(maxWidth: .infinity)
+                    .padding(.top, 8)
+                    .background(.ultraThinMaterial)
+                
+            } else {
+                Color.ember
+                contentStack()
+                    .frame(height: layout.size * 0.8)
+            }
+            
+            Text(moment.timestamp.formatted(
+                .dateTime
+                    .month(.abbreviated).day()))
+            .font(.footnote)
+            .padding(.bottom, layout.size * 0.08)
+            .frame(maxWidth: layout.size / 3)
+            .frame(maxHeight: layout.timestampHeigh)
+            
+        }
+        .foregroundStyle(.white)
+
+    }
+    
+    private func contentStack() -> some View {
+        VStack(alignment: .leading) {
+            Text(moment.title)
+                .font(layout.titleFont)
+            
+            if !moment.note.isEmpty, !showImage {
+                Text(moment.note)
+                    .font(layout.bodyFont)
+            }
+        }
+        .frame(maxWidth: layout.size * 0.8)
+        .frame(maxHeight: layout.size * (showImage ? 0.15 : 0.5))
+        .padding(.bottom, layout.size * layout.textBottomPadding)
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+#Preview {
+    ScrollView {
+        MomentHexagonView(moment: Moment.imageSample)
+        MomentHexagonView(moment: Moment.imageSample, layout: .large)
+        MomentHexagonView(moment: Moment.sample)
+        MomentHexagonView(moment: Moment.sample, layout: .large)
+    }
+}
