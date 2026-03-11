1de6365d303df43feebc1810d0a3e9209f5aa62e
RP <hupochuan80@icloud.com>
2026-03-11 08:22:40 +0000

    Build a detail view

---
+++ Develop in Swift/GratefulMoments/GratefulMoments/Tabs/Achievements/BadgeDetailView.swift
@@ -0,0 +1,38 @@
+//
+//  BadgeDetailView.swift
+//  GratefulMoments
+//
+//  Created by hh on 11/03/2026.
+//
+
+import SwiftUI
+
+struct BadgeDetailView: View {
+    var badge: Badge
+    
+    var body: some View {
+        VStack {
+            Image(badge.details.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 140, height: 140)
+            Text(badge.details.title)
+                .font(.title.bold())
+            Text(badge.details.congratulatoryMessage)
+                .font(.body)
+            
+            Spacer()
+        }
+        .padding()
+        .frame(width: 320, height: 410)
+        .multilineTextAlignment(.center)
+        .foregroundStyle(.white)
+        .background(badge.details.color.opacity(0.8))
+        .clipShape(RoundedRectangle(cornerRadius: 16.0))
+        
+    }
+}
+
+#Preview {
+    BadgeDetailView(badge: .sample)
+}
